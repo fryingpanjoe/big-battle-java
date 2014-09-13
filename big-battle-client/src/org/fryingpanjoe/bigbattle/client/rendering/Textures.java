494b6d5ee65ab49353d0e041e29a02916c074780
Per Lindstrand <fpj@spotify.com>
2014-09-13 18:23:18 +0200

    use clamp to edge for s,t wrapping

--- big-battle-client/src/org/fryingpanjoe/bigbattle/client/rendering/Textures.java
+++ big-battle-client/src/org/fryingpanjoe/bigbattle/client/rendering/Textures.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.commons.io.FilenameUtils;
 import org.fryingpanjoe.bigbattle.common.io.Files;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -17,8 +18,8 @@ public class Textures {
     final Texture texture = TextureLoader.getTexture(
       FilenameUtils.getExtension(filename).toUpperCase(), Files.getFileAsStream(filename));
     texture.bind();
-    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
     GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
     GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
     return texture;
