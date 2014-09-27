84d64017e67ab8687c546f45a5baecf91611f13b
Per Lindstrand <fpj@spotify.com>
2014-09-27 15:54:22 +0200

    add 'fist' weapon

--- big-battle-common/src/org/fryingpanjoe/bigbattle/common/game/Weapons.java
+++ big-battle-common/src/org/fryingpanjoe/bigbattle/common/game/Weapons.java
@@ -7,18 +7,27 @@ import com.google.common.collect.ImmutableMap;
 public class Weapons {
 
   public enum WeaponId {
+    Fist,
     Stick,
   }
 
-  public static final Weapon STICK = new Weapon(
-    WeaponId.Stick.ordinal(), // id
+  public static final Weapon FIST = new Weapon(
+    WeaponId.Fist.ordinal(), // id
     1.f, // damage
     1.f, // range
     1.f // delay
   );
 
+  public static final Weapon STICK = new Weapon(
+    WeaponId.Stick.ordinal(), // id
+    10.f, // damage
+    1.5f, // range
+    1.f // delay
+  );
+
   private static final Map<WeaponId, Weapon> ALL = ImmutableMap.of(
-    WeaponId.Stick, STICK);
+    WeaponId.Stick, STICK,
+    WeaponId.Fist, FIST);
 
   private Weapons() {
   }
